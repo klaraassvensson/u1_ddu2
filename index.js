4e0193b1cd1762fb70951269873893f196b1d984
klaraassvensson <klara.svensson04@gmail.com>
2024-11-19 12:43:01 +0100

    list of cities and the users input
    Added the boxes with city names and the users city, from prompt. h2 changes depending on the information in the database

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 // Recommended: All functions declared here
+function allCities (userCity) {
+    for (let city of cities){
+        if (userCity == city.name){
+            return city;
+        }
+    }
+    return null;
+}
+
 
 // Recommended: constants with references to existing HTML-elements
+const citiesDiv = document.getElementById("cities");
+const h2 = document.querySelector("h2");
 
 // Recommended: Ask for the city name and then the rest of the code
+const userCity = prompt("Ange en stad i Europa!")
 
+if (allCities(userCity) == null){
+    h2.textContent = userCity + " finns inte i databasen"; 
+} else {
+    h2.textContent = userCity + " (" + allCities(userCity).country  + ")"; 
+}
+
+for (let city of cities){
+    citiesDiv.innerHTML += `<p class="cityBox">${city.name}</p>`;
+}
