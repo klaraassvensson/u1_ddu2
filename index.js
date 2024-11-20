d4b0064d9e9edd1b0faeda3a37caf5e2a3647336
klaraassvensson <klara.svensson04@gmail.com>
2024-11-20 12:10:05 +0100

    search for nearest and furthest city
    this is the second version, using for-loops, functions and if-declarations.

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ function allCities (whichCity) {
     return null;
 }
 
+function fFindDistancesFromCity (cityId){
+    return distances.filter(d => d.city == cityId || d.city2 == cityId)
+}
+
+function getCityById (id) {
+    return cities.find(city => city.id == id)
+}
 
 // Recommended: constants with references to existing HTML-elements
 const citiesDiv = document.getElementById("cities");
@@ -17,20 +24,48 @@ const tableDiv = document.getElementById("table");
 
 // Recommended: Ask for the city name and then the rest of the code
 const userCity = prompt("Ange en stad i Europa!")
+const foundCity = allCities(userCity)
 
-if (allCities(userCity) == null){
+if (foundCity == null){
     h2.textContent = userCity + " finns inte i databasen"; 
     document.title = "Not found"
     h3.remove();
 } else {
-    h2.textContent = userCity + " (" + allCities(userCity).country  + ")"; 
+    h2.textContent = userCity + " (" + foundCity.country  + ")"; 
     document.title = userCity;
 }
 
-for (let city of cities){
-    citiesDiv.innerHTML += `<p class="cityBox">${city.name}</p>`;
-}
 
+let closestCity = null;
+let furthestCity = null;
+let minDistance = Infinity;
+let maxDistance = 0;
+
+for (let d of distances){
+    const otherCityId = d.city1 == foundCity.id ? d.city2 : d.city1;
+    const otherCity = getCityById(otherCityId);
+    if (d.distance < minDistance){
+        minDistance = d.distance;
+        closestCity = otherCity;
+    }
+    if (d.distance > maxDistance){
+        maxDistance = d.distance;
+        furthestCity = otherCity;
+    }
+}
+h3.textContent = `Av städerna i databasen ligger ${closestCity.name} närmast och ${furthestCity.name} längst bort`;
+
+for (let city of cities){
+    let cityClass = "";
+    if (city.name == userCity){
+        cityClass = "target";
+    } else if (city.id == closestCity.id){
+        cityClass = "closest";
+    } else if (city.id == furthestCity.id){
+        cityClass = "furthest";
+    }
+    citiesDiv.innerHTML += `<p class="cityBox ${cityClass}">${city.name}</p>`;
+}
 
 tableDiv.innerHTML = `<p class="cell"></p>`
 
