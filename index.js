2ee32625c0883b0e49b42d5ea6faa771be25076e
klaraassvensson <klara.svensson04@gmail.com>
2024-11-25 11:26:51 +0100

    search for furthest and closest city works

--- index.js
+++ index.js
@@ -27,25 +27,26 @@ if (foundCity == null){
     document.title = userCity;
     
 }
-
-
 let closestCity = null;
 let furthestCity = null;
 let minDistance = Infinity;
 let maxDistance = 0;
 if (foundCity != null){
     for (let d of distances){
-    const otherCityId = d.city1 == foundCity.id ? d.city2 : d.city1;
-    const distance = d.distance;
-        if (distance < minDistance){
-            minDistance = distance;
-            closestCity = cities.find(city => city.id == otherCityId);
+        if (d.city1 == foundCity.id || d.city2 == foundCity.id) {
+            const otherCityId = d.city1 == foundCity.id ? d.city2 : d.city1;
+            const distance = d.distance;
+                if (distance < minDistance){
+                    minDistance = distance;
+                    closestCity = cities.find(city => city.id == otherCityId);
+                }
+                if (distance > maxDistance){
+                    maxDistance = distance;
+                    furthestCity = cities.find(city => city.id == otherCityId);
+                }
+            }
+
         }
-        if (distance > maxDistance){
-            maxDistance = distance;
-            furthestCity = cities.find(city => city.id == otherCityId);
-        }
-    }
     h3.textContent = `Av städerna i databasen ligger ${closestCity.name} närmast och ${furthestCity.name} längst bort`;
     
     for (let city of cities){
